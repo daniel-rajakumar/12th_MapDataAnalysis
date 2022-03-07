7dedd66cb0fb5fdd4f681c3a721fcaf6f46c9451
kingDaniel2004 <danielelvisr@gmail.com>
2022-03-07 16:18:35 -0500

    made textview on home screen.

--- src/main/java/com/daniel/data_mapping/HomeView.java
+++ src/main/java/com/daniel/data_mapping/HomeView.java
@@ -6,39 +6,52 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.Arrays;
 
 @PageTitle("Home")
 @Route("home")
 @JsModule("./script.js")
 @JsModule("./headerScript.js")
 @CssImport(value = "home-style.css")
-public class HomeView extends VerticalLayout {
+public class HomeView extends FlexLayout {
+    String[] tabTitles = {"Topic", "Hypothesis", "Result", "Conclusion", "Citation"};
 
     public HomeView() {
-        Tab details = new Tab("Details");
-        Tab payment = new Tab("Payment");
-        Tab shipping = new Tab("Shipping");
+        setHeightFull();
+//        setPadding(false);
+        Button button = new Button("click me");
+        button.getElement().getStyle().set("margin-top", "100px");
+        Label textField = new Label("hello world, how are you doing? I think you are doing great. This is cool. one two three four");
+//        textField.setSizeFull();
+        textField.setClassName("home-Label");
+        FlexLayout flexLayout = new FlexLayout(setUpTabs(tabTitles), textField);
+//        flexLayout.setFlexGrow(30);
+//        flexLayout.setFlexBasis("30px");
+        add(flexLayout);
+    }
 
-        Tabs tabs = new Tabs(details, payment, shipping);
+    Tabs setUpTabs(String... tab){
+        Tabs tabs = new Tabs();
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         tabs.getElement().getStyle().set("margin-top", "85px");
         tabs.getElement().getStyle().set("background-color", "#021721");
-        setPadding(false);
-        tabs.setHeight("1000px");
-        tabs.setWidth("250px");
+//        tabs.setHeight("1000px");
+//        tabs.setHeightFull();
+        tabs.setWidth("300px");
 
-
-
-        Button button = new Button("click me");
-        button.getElement().getStyle().set("margin-top", "100px");
-//        add(new HorizontalLayout(new Button(), tabs));
-        add(new HorizontalLayout(tabs,button));
+        Arrays.stream(tab).forEach(e -> tabs.add(new Tab(e)));
+        return tabs;
     }
+
 
 }
